86211fdc6dad66e26404447e1495969e569d1edf
Cheyi <i821209n@gmail.com>
2022-05-31 22:04:55 +0800

    1. refactor product category from String to Enum

--- src/main/java/com/cylin/springbootmall/rowMapper/ProductRowMapper.java
+++ src/main/java/com/cylin/springbootmall/rowMapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.cylin.springbootmall.rowMapper;
 
+import com.cylin.springbootmall.constant.ProductCategory;
 import com.cylin.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,7 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProduct_id(resultSet.getInt("product_id"));
         product.setProduct_name(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
         product.setImage_url(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
