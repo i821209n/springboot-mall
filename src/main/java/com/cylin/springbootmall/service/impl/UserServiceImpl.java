ca78fb10c919012a26d4e7027c40b147642c75a0
Cheyi <i821209n@gmail.com>
2022-07-09 17:28:18 +0800

    1. encrypt password

--- src/main/java/com/cylin/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/cylin/springbootmall/service/impl/UserServiceImpl.java
@@ -11,7 +11,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class UserServiceImpl implements UserService {
@@ -35,6 +38,9 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         return userDao.createUser(userRegisterRequest);
     }
 
@@ -47,7 +53,9 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        if(user.getPassword().equals(hashedPassword)){
             return user;
         } else {
             log.warn("login failed. password is wrong");
